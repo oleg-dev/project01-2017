3e556d27fddb54800b573c5c7e2d0f0153499f8c
oleg-dev <oleg.merg.develop@gmail.com>
2017-01-25 19:57:27 +0200

    update(styles, adaptation styles):update adaptation styles

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 
 		//узнаем высоту от начала страницы до блока на который ссылается якорь
 			top = $(id).offset().top;
-		
+
 		//анимируем переход на расстояние - top за 1500 мс
 		$('body,html').animate({scrollTop: top}, 1500);
 	});
@@ -22,6 +22,19 @@ $(document).ready(function(){
 		//узнаем высоту от начала страницы до блока на который ссылается якорь
 			top = $(id).offset().top;
 		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+	$("#sandwich-menu").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+
 		//анимируем переход на расстояние - top за 1500 мс
 		$('body,html').animate({scrollTop: top}, 1500);
 	});
