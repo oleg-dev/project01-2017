f5dc0458c1cc9966c7c8cdab0141092056d8354b
Oleh Merhuriev <oleg.merg@gmail.com>
2017-01-26 21:49:31 +0200

    fix(menu):fix menu view on mobile phones

--- js/script.js
+++ js/script.js
@@ -18,6 +18,31 @@ $(document).ready(function(){
 			top = $(id).offset().top;
 		$('body,html').animate({scrollTop: top}, 1500);
 	});
-	$('.nav-2').hide();
-	$('#mobile-menu').click(function(){$(this).next().toggle()});
+
+
+	/*===for mobile====*/
+
+	$("#menu").on("tap","a", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+	});	
+	$("#menu-footer").on("tap","a", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+	$("#sandwich-menu").on("tap","a", function (event) {
+		$('.nav-2').hide();
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+	
+});
+$(document).scroll(function(){
+    $('.nav-2').hide();
 });
